e0309aba88f9735051cda20e1fe8f92a77c7c48d
Ura-sapfir <Ura-sapfir@HOME-PC>
2023-06-09 00:53:19 +0500

    Добавьте файлы проекта.

---
+++ DZ-2_oop/Source.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+#include<string>
+#include<vector>
+
+
+class Human {
+private:
+	char* _Fio;
+	int _Age;
+public:
+	Human(std::string fio,int age):_Age(age){
+		_Fio = new char[fio.length()];
+		for (int index = 0; index < fio.length(); ++index)
+			_Fio[index] = fio[index];
+	}
+
+	int GetHuman()
+	{
+
+	}
+
+	~Human()
+	{
+		delete[]_Fio;
+	}
+
+};
+
+class Room : protected House {
+private:
+	
+	int _Quantity;
+	std::vector<Human*>_Room;
+
+public:
+	Room(std::vector<Human*>room) :_Room(room){}
+
+	void AddHumanByRoom(Human* resident)
+	{
+		_Room.push_back(resident);
+	}
+	int GetQuantity()
+	{
+		std::cout << " Quantity resident = " << _Room.size() + 1 << '\n';
+	}
+	int GetNumber()
+	{
+		return _Number;
+	}
+
+};
+
+class House {
+private:
+	int *_House = new int[1];
+protected:
+	int _Number=1;
+public:
+	House(Room*) {
+		
+
+	}
+
+};
+
+
